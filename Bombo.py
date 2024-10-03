ed4dfbaba727729c7ca445d77a51601986ccbe0f
JustSomeoneHere72 <nicolasarboledaestudios@gmail.com>
2024-10-03 18:40:13 +0200

    Update Bombo.py

--- Bombo.py
+++ Bombo.py
@@ -1,1 +1,16 @@
+import random
 
+def mostrar_palabra_aleatoria():
+    # Lista de palabras
+    palabras = [
+        "manzana", "banana", "cereza", "durazno", "kiwi",
+        "naranja", "pera", "sandía", "uva", "mora",
+        "ananas", "coco", "mango", "limón", "fresa"
+    ]
+
+    # Seleccionar una palabra aleatoria
+    palabra_aleatoria = random.choice(palabras)
+    print(f"Palabra aleatoria: {palabra_aleatoria}")
+
+# Ejecutar la función
+mostrar_palabra_aleatoria()
